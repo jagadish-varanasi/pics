87c8fa3a1e8825eb34b488eb4102afb243df9a56
vjagadishvaranasi <jagadishvaranasi123@gmail.com>
2020-05-06 22:15:56 +0530

    adding arrow func for event and removing sep func

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -3,9 +3,9 @@ import React from 'react';
 class SearchBar extends React.Component{
 //callback is called ...event object contains buch of uinfo that has just occured
 // onInputChange is wantedly given name...Input is the element and chnge is the event...its is communiuty convention to make more meaningful and understandable
-    onInputChange(event){
-        console.log(event.target.value)
-    }
+    // onInputChange(event){
+    //     console.log(event.target.value)
+    // }
 
     // onInputClick(){
     //     console.log('The input was clicked!!')
@@ -19,7 +19,7 @@ class SearchBar extends React.Component{
                 <form className="ui form">
                    <div className="field">
                     <label>Image Search</label>   
-                    <input type="text" onChange={this.onInputChange}/>
+                    <input type="text" onChange={(event)=>console.log(event.target.value)}/>
                     {/* <input type='text' onClick={this.onInputClick} /> */}
                                       {/* onChange is a prop name...different events are goinging to wired to difff prop.... like onClick..onSubmit*/}
                     {/* u should not put onInputChange() parantesis bcz...if u put that as soon as render method is called onInputChange is also called.That should not happen ..so it should be a callback func that is called only when input is given*/}
