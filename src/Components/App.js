9d72f2d82cff5515239a4fa4edb73141bb208c7f
vjagadishvaranasi <jagadishvaranasi123@gmail.com>
2020-05-10 22:30:30 +0530

    added image list

--- src/Components/App.js
+++ src/Components/App.js
@@ -18,7 +18,7 @@ class App extends React.Component{
    return(
        <div className="ui container" style={{marginTop:'10px'}}>
            <SearchBar onSubmit={this.onSearchSubmit}/>
-            <ImageList iamge={this.state.images}/>
+            <ImageList image={this.state.images}/>
        </div>
    );
    }
--- src/Components/ImageList.js
+++ src/Components/ImageList.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
 const ImageList =(props)=>{
-    console.log(props);
-  return <div>ImageList</div>
+    const images=props.image.map((image)=>{
+       return <img alt={image.description} src={image.urls.regular} key={image.id} />
+    });
+return <div>{images}</div>
 };
 
 export default ImageList;
