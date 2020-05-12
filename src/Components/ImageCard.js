77d44835ccb816e7da4108de268df4482e2469e5
vjagadishvaranasi <jagadishvaranasi123@gmail.com>
2020-05-12 23:38:46 +0530

    creating image ref

--- src/Components/ImageCard.js
+++ src/Components/ImageCard.js
@@ -2,18 +2,31 @@ import React from 'react';
 //import ImageList from './ImageList';
 
 class ImageCars extends React.Component{
+    constructor(props){
+        super(props);
+
+        this.imageRef=React.createRef();
+    }
+
+    componentDidMount(){
+        console.log(this.imageRef);
+       // ull get undefined or 0 bcz...u just not loaded image..later u can see clientHeight in inspect...
+        console.log(this.imageRef.current.clientHeigth);
+    }
 
     render(){
         //const {description,urls}=this.props.image;
         return( <div>
         <img
+            //  ref is jsx tag its not a dom element
+            ref={this.imageRef}
             alt={this.props.image.description}
             src={this.props.image.urls.regular}
         
         />
             {/* <img
             alt={description}
-            src={this.props.image.urls.regular}
+            src={regular}
         
         /> */}
 
--- src/Components/ImageList.js
+++ src/Components/ImageList.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './imageList.css';
+import ImageCard from './ImageCard'; 
 
 const ImageList =(props)=>{
     const images=props.image.map((image)=>{
-       return <img alt={image.description} src={image.urls.regular} key={image.id} />
+       return <ImageCard image={image}  key={image.id} />
     });
 return <div className="image-list">{images}</div>
 };
