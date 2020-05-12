d1a52a6f09d29790c73db2a21168d2aa96266909
vjagadishvaranasi <jagadishvaranasi123@gmail.com>
2020-05-12 23:53:25 +0530

    added callback

--- src/Components/ImageCard.js
+++ src/Components/ImageCard.js
@@ -4,19 +4,25 @@ import React from 'react';
 class ImageCars extends React.Component{
     constructor(props){
         super(props);
-
+       this.state={spans:0};
         this.imageRef=React.createRef();
     }
 
     componentDidMount(){
-        console.log(this.imageRef);
        // ull get undefined or 0 bcz...u just not loaded image..later u can see clientHeight in inspect...
-        console.log(this.imageRef.current.clientHeigth);
+        this.imageRef.current.addEventListener('load',this.setSpans);
+    }
+
+    //setSpan callback function
+    setSpans=()=>{
+        const height=this.imageRef.current.clientHeight;
+        const spans=Math.ceil(height/10);
+        this.setState({spans});
     }
 
     render(){
         //const {description,urls}=this.props.image;
-        return( <div>
+        return( <div style={{gridRowEnd:`span ${this.state.spans}`}}>
         <img
             //  ref is jsx tag its not a dom element
             ref={this.imageRef}
